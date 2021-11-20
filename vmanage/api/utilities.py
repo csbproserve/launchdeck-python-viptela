26930e1664890964e730f2406e89e7203daee8d9
Jeremy Sanders <23668453+jeremypng@users.noreply.github.com>
2021-11-19 22:05:14 -0600

    Revert "removing duplicated code in utilities.Utilities.waitfor_action_completion"
    This reverts commit 06efd8bfb6dab4bcf942417d718db08b309b7875.

--- vmanage/api/utilities.py
+++ vmanage/api/utilities.py
@@ -87,11 +87,33 @@ class Utilities(object):
         action_status = None
         action_activity = None
         action_config = None
-        while status == 'in_progress':
-            action_status = self.get_action_status(action_id)
-            status = action_status['action_response']['summary']['status']
+        while status == "in_progress":
+            url = f"{self.base_url}device/action/status/{action_id}"
+            response = HttpMethods(self.session, url).request('GET')
+            ParseMethods.parse_data(response)
 
-        return 
+            if 'json' in response:
+                status = response['json']['summary']['status']
+                if 'data' in response['json'] and response['json']['data']:
+                    action_status = response['json']['data'][0]['statusId']
+                    action_activity = response['json']['data'][0]['activity']
+                    if 'actionConfig' in response['json']['data'][0]:
+                        action_config = response['json']['data'][0]['actionConfig']
+                    else:
+                        action_config = None
+                else:
+                    action_status = status
+            else:
+                raise Exception(msg="Unable to get action status: No response")
+            time.sleep(10)
+
+        return {
+            'action_response': response['json'],
+            'action_id': action_id,
+            'action_status': action_status,
+            'action_activity': action_activity,
+            'action_config': action_config
+        }
 
     def upload_file(self, input_file):
         """Upload a file to vManage.
