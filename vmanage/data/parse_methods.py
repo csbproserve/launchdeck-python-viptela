09994aa166d9c5d398c85894c625eabfdc2087b8
Jeremy Sanders <23668453+jeremypng@users.noreply.github.com>
2023-12-04 17:22:37 -0600

    adding parse_devices method to ParseMethods

--- vmanage/data/parse_methods.py
+++ vmanage/data/parse_methods.py
@@ -32,6 +32,28 @@ class ParseMethods:
             result = response['details']
             raise Exception(f'{error}: {result}')
         return result
+    
+    @staticmethod
+    def parse_devices(response):
+        """Parse devices and provide error handling for missing data.
+
+        Args:
+            response (obj): Requests response object
+
+        Returns:
+            result (dict): All data associated with a response.
+
+        Raises:
+            Exception: Provides error message and details of issue.
+        """
+
+        if response['json'] and 'devices' in response['json']:
+            result = response['json']['devices']
+        else:
+            error = response['error']
+            result = response['details']
+            raise Exception(f'{error}: {result}')
+        return result
 
     @staticmethod
     def parse_status(response):
